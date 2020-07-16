11c3e1ebb1a647c31c4cad473cab068b0169d065
zorojurio <zorojurio@gmail.com>
2020-07-16 20:45:41 +0530

    final

--- Pract/urls.py
+++ Pract/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 # inorder to display images
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
